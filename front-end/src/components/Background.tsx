b0b6c023fc907c3b9533a39f58278c54b5f6a461
Kevin Zahn <kevin.zahn@bluewin.ch>
2023-02-02 23:41:16 +0100

    Add Background.tsx. Closes #2

---
+++ front-end/src/components/Background.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from "react";
+
+type Props = {
+    className: string;
+    children?: React.ReactNode
+};
+
+const Background: FC<Props> = ({children, className}) => {
+    const style = "h-screen w-screen bg-gradient-to-r from-dark-blue to-blue " + className;
+
+    return (
+        <div className={style}>
+            {children}
+        </div>);
+}
+
+export default Background;
