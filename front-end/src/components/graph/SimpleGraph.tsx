fa3db2190355752cfc37ffc58b96578429b585d9
Kevin Zahn <kevin.zahn@bluewin.ch>
2023-02-05 22:26:01 +0100

    Comment out helper lines. Closes #5

--- front-end/src/components/graph/SimpleGraph.tsx
+++ front-end/src/components/graph/SimpleGraph.tsx
@@ -1,5 +1,7 @@
 import React, {FC} from "react";
 
+const y_offset = 8;
+
 type Data = {
     x: number;
     y: number;
@@ -8,21 +10,19 @@ type Data = {
 
 const SimpleGraph: FC<Data> = ({x, y, data}) => {
 
-    const y_offset = 8;
     y = y + y_offset;
 
     const y_middle = y / 2;
-
     const x_gap = x / (data.length - 1);
     const y_average = data.reduce( ( p, c ) => p + c, 0 ) / data.length; // Calc Average
-
     const y_data_offset = y_middle - y_average
 
     return (
         <svg>
-            <line x1={0} x2={x} y1={y_offset} y2={y_offset} style={{stroke: "red"}}></line>
-            <line x1={0} x2={0} y1={0} y2={y} style={{stroke: "pink"}}></line>
-            <line x1={0} x2={x} y1={y_middle} y2={y_middle} style={{stroke: "pink"}}></line>
+            {/*Helper Lines*/}
+            {/*<line x1={0} x2={x} y1={y_offset} y2={y_offset} style={{stroke: "red"}}></line>*/}
+            {/*<line x1={0} x2={0} y1={0} y2={y} style={{stroke: "pink"}}></line>*/}
+            {/*<line x1={0} x2={x} y1={y_middle} y2={y_middle} style={{stroke: "pink"}}></line>*/}
             {
                 data.map((value, index, array) => {
                     if (index !== data.length - 1) {
