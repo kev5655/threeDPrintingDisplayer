46d1a807fbe11ce6359a6191f1f9cc7bf2859810
Kevin Zahn <kevin.zahn@bluewin.ch>
2023-02-22 16:22:02 +0100

    add Spring boot application

---
+++ back-end/src/main/java/kev/threeDPrintingDisplayer/ThreeDPrintingDisplayerApplication.java
@@ -0,0 +1,13 @@
+package kev.threeDPrintingDisplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeDPrintingDisplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreeDPrintingDisplayerApplication.class, args);
+	}
+
+}
